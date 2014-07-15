df85ea51e5a928eb04022dd56d8804ba80a0a1fd
Radhika Sampath <rsampath@mobiquityinc.com>
2014-07-15 15:33:01 -0400

    Lab: Flexible Dimensions

--- src/test/java/com/example/Calculator/ButtonFragmentTest.java
+++ src/test/java/com/example/Calculator/ButtonFragmentTest.java
@@ -7,6 +7,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
 import static com.support.Assert.Assert.assertViewIsVisible;
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -41,19 +42,50 @@ public class ButtonFragmentTest {
     {
         /*assertViewIsVisible( buttonFragment.getView()
                 .findViewById( R.id.key1 ) );*/
-        TextView key1 = (TextView) buttonFragment.getView().findViewById( R.id.key1 );
-        assertViewIsVisible( key1 );
-        assertThat( key1.getText().toString(),
-                equalTo( ResourceLocator.getString(R.string.key1) ) );
+        verifyOneKey();
+
 
     }
-
     @Test
-    public void shouldHaveAllKeys() throws Exception {
+    @Config(qualifiers = "ja")
+    public void shouldHaveJapaneseKeys()
+    {
         int[] keys = new int[]{R.id.key0,R.id.key1,R.id.key2,R.id.key3,R.id.key4,R.id.key5,R.id.key6,R.id.key7,R.id.key8,
                 R.id.key9,R.id.keyAdd,R.id.keySub,R.id.keyMul,R.id.keyDiv,R.id.keyMod,R.id.keyClear,R.id.keyEqual};
         for (int i = 0; i < keys.length; i++) {
             assertViewIsVisible(buttonFragment.getView().findViewById(keys[i]));
         }
+    }
+
+    private void verifyOneKey()
+    {
+        TextView key1 = (TextView) buttonFragment.getView().findViewById( R.id.key1 );
+        assertViewIsVisible( key1 );
+        assertThat( key1.getText().toString(),
+                equalTo( ResourceLocator.getString(R.string.key1) ) );
+    }
+
+    @Test
+    public void shouldHaveAllKeys() throws Exception {
+
+        int[] keys = new int[]{R.id.key0,R.id.key1,R.id.key2,R.id.key3,R.id.key4,R.id.key5,R.id.key6,R.id.key7,R.id.key8,
+                R.id.key9,R.id.keyAdd,R.id.keySub,R.id.keyMul,R.id.keyDiv,R.id.keyMod,R.id.keyClear,R.id.keyEqual};
+        for ( int i =0; i< keys.length;i++) {
+            assertViewIsVisible(buttonFragment.getView().findViewById(keys[i]));
+
+        int[] numbers = new int[]{R.id.key0,R.id.key1,R.id.key2,R.id.key3,R.id.key4,R.id.key5,R.id.key6,R.id.key7,R.id.key8,
+                    R.id.key9};
+
+        String [] symbols = new String[]{"零", "一","二","三","四","五","六","七","八","九"};
+            for(int j = 0; j< symbols.length; j++){
+                TextView key = (TextView) buttonFragment.getView().findViewById( numbers[j] );
+                assertThat(key.getText().toString(),equalTo(symbols[j]));
+
+            }
+        }
+
+
+
+
     }
 }
