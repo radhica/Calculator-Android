9b4ae19dfbb08f1cd4992cf068f72f5839266867
Radhika Sampath <rsampath@mobiquityinc.com>
2014-07-15 09:19:39 -0400

    commiting changes to ButtonFragmentTest shouldHaveAllKeys

--- src/test/java/com/example/Calculator/ButtonFragmentTest.java
+++ src/test/java/com/example/Calculator/ButtonFragmentTest.java
@@ -1,11 +1,17 @@
 package com.example.Calculator;
 
+import android.widget.TextView;
 import com.support.Assert.FragmentUtil;
+import com.support.Assert.ResourceLocator;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 
+import static com.support.Assert.Assert.assertViewIsVisible;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 
 /**
  * Created by rsampath on 7/14/14.
@@ -14,10 +20,40 @@ import static org.junit.Assert.assertNotNull;
 @RunWith(RobolectricTestRunner.class)
 public class ButtonFragmentTest {
 
+    private ButtonFragment buttonFragment;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        buttonFragment = new ButtonFragment();
+        FragmentUtil.startFragment(buttonFragment);
+    }
+
     @Test
     public void shouldNotBeNull() throws Exception{
-        ButtonFragment fragment = new ButtonFragment();
-        FragmentUtil.startFragment(fragment);
-        assertNotNull( fragment );
+       // ButtonFragment fragment = new ButtonFragment();
+        //FragmentUtil.startFragment(fragment);
+        assertNotNull( buttonFragment );
+    }
+
+    @Test
+    public void shouldHaveOneKey() throws Exception
+    {
+        /*assertViewIsVisible( buttonFragment.getView()
+                .findViewById( R.id.key1 ) );*/
+        TextView key1 = (TextView) buttonFragment.getView().findViewById( R.id.key1 );
+        assertViewIsVisible( key1 );
+        assertThat( key1.getText().toString(),
+                equalTo( ResourceLocator.getString(R.string.key1) ) );
+
+    }
+
+    @Test
+    public void shouldHaveAllKeys() throws Exception {
+        int[] keys = new int[]{R.id.key0,R.id.key1,R.id.key2,R.id.key3,R.id.key4,R.id.key5,R.id.key6,R.id.key7,R.id.key8,
+                R.id.key9,R.id.keyAdd,R.id.keySub,R.id.keyMul,R.id.keyDiv,R.id.keyMod,R.id.keyClear,R.id.keyEqual};
+        for (int i = 0; i < keys.length; i++) {
+            assertViewIsVisible(buttonFragment.getView().findViewById(keys[i]));
+        }
     }
 }
--- target/generated-sources/r/com/example/Calculator/R.java
+++ target/generated-sources/r/com/example/Calculator/R.java
@@ -17,9 +17,26 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int button_fragment=0x7f060002;
-        public static final int display=0x7f060000;
-        public static final int display_fragment=0x7f060001;
+        public static final int button_fragment=0x7f060013;
+        public static final int display=0x7f060011;
+        public static final int display_fragment=0x7f060012;
+        public static final int key0=0x7f060009;
+        public static final int key1=0x7f060000;
+        public static final int key2=0x7f060001;
+        public static final int key3=0x7f060002;
+        public static final int key4=0x7f060003;
+        public static final int key5=0x7f060004;
+        public static final int key6=0x7f060005;
+        public static final int key7=0x7f060006;
+        public static final int key8=0x7f060007;
+        public static final int key9=0x7f060008;
+        public static final int keyAdd=0x7f06000b;
+        public static final int keyClear=0x7f06000f;
+        public static final int keyDiv=0x7f06000e;
+        public static final int keyEqual=0x7f060010;
+        public static final int keyMod=0x7f06000a;
+        public static final int keyMul=0x7f06000d;
+        public static final int keySub=0x7f06000c;
     }
     public static final class layout {
         public static final int buttons=0x7f030000;
@@ -28,5 +45,22 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f050000;
+        public static final int key0=0x7f05000a;
+        public static final int key1=0x7f050001;
+        public static final int key2=0x7f050002;
+        public static final int key3=0x7f050003;
+        public static final int key4=0x7f050004;
+        public static final int key5=0x7f050005;
+        public static final int key6=0x7f050006;
+        public static final int key7=0x7f050007;
+        public static final int key8=0x7f050008;
+        public static final int key9=0x7f050009;
+        public static final int keyAdd=0x7f05000b;
+        public static final int keyClear=0x7f050010;
+        public static final int keyDiv=0x7f05000e;
+        public static final int keyEqual=0x7f050011;
+        public static final int keyMod=0x7f05000f;
+        public static final int keyMul=0x7f05000d;
+        public static final int keySub=0x7f05000c;
     }
 }
